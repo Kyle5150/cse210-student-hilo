b3d4fc7990c8c1acb9e1ec515d73c68bd93ef6e3
BenPainter <ben4painter@gmail.com>
2021-10-07 14:57:25 -0600

    Update player.py

--- hilo/game/player.py
+++ hilo/game/player.py
@@ -1,1 +1,12 @@
+from game.dealer import Dealer
+from random import randint
 
+
+class Player:
+
+    def __init_(self):
+        self.old_card = 1
+        self.new_card = 1
+        self.can_draw = True
+
+        
