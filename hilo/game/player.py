22ef84950bdeac1188055359e06c8c8beaaeb003
nreed31415 <91569412+nreed31415@users.noreply.github.com>
2021-10-08 13:50:56 -0600

    Update player.py
    it works now

--- hilo/game/player.py
+++ hilo/game/player.py
@@ -4,18 +4,20 @@ from random import randint
 class Player:
 
     def __init__(self):
-        self.next_card = 1
+        self.next_card = randint(1,13)
         self.first_card = 1
         self.can_draw = True
 
         
     def draw(self):
-        self.first_card = randint(1,13)
+        self.first_card = self.next_card
         self.next_card = randint(1,13)
 
     def draw_able(self, score):
         if score <= 0:
             self.can_draw = False
+        else: 
+            return True
 
     def get_points(self, compare):
         if compare == True:
