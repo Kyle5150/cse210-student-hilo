8c0065521f53f110d1eb6ab57b1ed86e08ec5e54
nreed31415 <91569412+nreed31415@users.noreply.github.com>
2021-10-08 13:03:24 -0600

    Update player.py
    changed some wording

--- hilo/game/player.py
+++ hilo/game/player.py
@@ -5,12 +5,14 @@ from random import randint
 class Player:
 
     def __init_(self):
-        self.old_card = 1
-        self.new_card = 1
+        self.next_card = 1
+        self.first_card = 1
         self.can_draw = True
 
         
     def draw():
+        self.first_card = randint(1,13)
+        self.next_card = rantint(1,13)
         pass
 
     def draw_able():
